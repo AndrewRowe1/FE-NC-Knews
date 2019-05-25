7f69d7cc677bfe7015f74247efab135e66122751
Andrewrowe1 <andrew_rowe1@yahoo.co.uk>
2019-05-25 22:01:46 +0100

    articles pagination added

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,10 +3,10 @@ import { getArticles } from '../api';
 import ArticlesList from './ArticlesList';
 
 class Articles extends Component {
-  state = { articles: null, loading: true, sort: null };
+  state = { articles: null, loading: true, sort: null, p: 1, total_count: 0 };
 
   render () {
-    const { articles, loading, sort } = this.state;
+    const { articles, loading, sort, p, total_count } = this.state;
     const sortMethods = ['created_at', 'comment_count', 'votes'];
     return loading ? <p>loading ...</p> : (
       <div>
@@ -14,10 +14,30 @@ class Articles extends Component {
           return <button disabled={sort === sorting} key={`sort by ${sorting}`} onClick={() => { this.sortArticles(sorting) }}>Sort Articles by {sorting === 'created_at' ? 'date' : sorting} </button>
         })}
         {sort ? <p>Articles sorted by {sort === 'created_at' ? 'date' : sort}</p> : null}
+        <button disabled={p === Math.ceil(total_count / 10)} onClick={() => { this.changePage(1) }} > More Articles</button >
+        <button disabled={this.state.p === 1} onClick={() => { this.changePage(-1) }} >Previous Articles</button>
         <ArticlesList articles={articles} />
       </div>
     );
   };
+
+  componentDidMount () {
+    getArticles()
+      .then((articles) => {
+        const { total_count } = articles[0];
+        this.setState({ articles, loading: false, total_count });
+      });
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.p !== this.state.p) {
+      getArticles({ p: this.state.p })
+        .then((articles) => {
+          const { total_count } = articles[0];
+          this.setState({ articles, loading: false, total_count });
+        });
+    }
+  }
 
   sortArticles = (sort) => {
     getArticles({ sort_by: sort })
@@ -26,11 +46,10 @@ class Articles extends Component {
       });
   }
 
-  componentDidMount () {
-    getArticles()
-      .then((articles) => {
-        this.setState({ articles, loading: false });
-      });
+  changePage = direction => {
+    this.setState(prevState => {
+      return { p: prevState.p + direction };
+    })
   }
 }
 
